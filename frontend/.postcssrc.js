3a2cd50552533148798cc1dc8d3e9642dfaebe44
Katsuragawa <andre.katsuragawa@gmail.com]>
2021-08-01 21:21:57 -0300

    update gitignore

--- frontend/.postcssrc.js
+++ frontend/.postcssrc.js
@@ -1,10 +1,11 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  plugins: {
     "postcss-import": {},
     "postcss-url": {},
+    tailwindcss: {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
+    autoprefixer: {},
+  },
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import VueAxios from 'vue-axios'
+import './main.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
